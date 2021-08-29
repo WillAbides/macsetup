815216901edaed45d27052bd3da93b7ac7a52c85
Will Roden <will@roden.cc>
2021-08-29 14:03:24 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
+
+CDPATH="" cd -- "$(dirname -- "$0")"
+
+# hold sudo until the script is done
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+if ! hash brew 2>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+brew update
+
+caffeinate -is brew bundle install
+
+if ! grep -q '/usr/local/bin/bash' /etc/shells; then
+  hold_sudo
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
+fi
+
+if [ "$SHELL" != '/usr/local/bin/bash' ]; then
+  hold_sudo
+  sudo chsh -s '/usr/local/bin/bash' "$(id -un)"
+fi
 
 dock_items='
 /System/Applications/Launchpad.app/
@@ -146,5 +170,5 @@ for app in "Activity Monitor" \
 	"Messages" \
 	"Photos" \
 	"SystemUIServer"; do
-	killall "${app}" &> /dev/null || true
+	killall "${app}" >/dev/null 2>&1 || true
 done
