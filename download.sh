de9ed0551887d2e83622e511553d622d26cce127
Will Roden <will@roden.cc>
2026-03-22 15:39:30 -0500

    update configuration and installation scripts; add support for new dependencies and casks

--- download.sh
+++ download.sh
@@ -10,8 +10,9 @@ TARGET="${TARGET:-"$HOME/repos/WillAbides/macsetup"}"
 git_url="https://github.com/WillAbides/macsetup.git"
 git_push_url="git@github.com:WillAbides/macsetup.git"
 
-if ! type brew >/dev/null 2>/&1; then
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! type brew >/dev/null 2>&1; then
+  install_brew="$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$install_brew"
 fi
 
 if [ -e "$TARGET" ]; then
